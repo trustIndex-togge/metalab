2532f137faa418cc18fe71f523e7b9739085f0e0
André Kalmendal <andre.kalmendal@lnu.se>
2022-02-23 15:29:11 +0100

    take out scatter plot

--- shinyapps/visualization/server.R
+++ shinyapps/visualization/server.R
@@ -288,50 +288,50 @@ shinyServer(function(input, output, session) {
 
   ########### SCATTER PLOT ###########
 
-  scatter <- function() {
-    req(input$scatter_curve)
-
-    labels <- if (mod_group() == "all_mod") NULL else
-      setNames(paste(mod_data()[[mod_group()]], "  "), mod_data()[[mod_group()]])
-
-    guide <- if (mod_group() == "all_mod") FALSE else "legend"
-    p <- ggplot(mod_data(), aes_string(x = "mean_age_months", y = es(),
-                                       colour = mod_group())) +
-      geom_jitter(aes(size = n, text = paste(short_cite, expt_num)), alpha = 0.5) +
-      geom_hline(yintercept = 0, linetype = "dashed", color = "grey") +
-      scale_colour_solarized(name = "", labels = labels, guide = guide) +
-      scale_size_continuous(guide = FALSE) +
-      xlab("\nMean Subject Age (Months)") +
-      ylab("Effect Size\n")
-
-    #curve <- if (is.null(categorical_mods())) input$scatter_curve else "lm"
-    if (input$scatter_curve == "lm") {
-      p <- p + geom_smooth(aes_string(weight = sprintf("1 / %s", es_var())),
-                      method = "lm", se = FALSE)
-    } else if (input$scatter_curve == "loess") {
-      p <- p + geom_smooth(aes_string(weight = sprintf("1 / %s", es_var())),
-                      method = "loess", se = FALSE, span = 1)
-    }
-
-    p <- ggplotly(p, tooltip = c("text"))
-
-    if (mod_group() != "all_mod") {
-      p
-    } else {
-      p %>%
-        layout(showlegend = FALSE)
-    }
-
-  }
-
-  output$scatter <- renderPlotly(scatter())
-
-  output$longitudinal <- reactive({
-    req(input$dataset_name)
-    filter(dataset_info, name == input$dataset_name)$longitudinal
-  })
-
-  outputOptions(output, "longitudinal", suspendWhenHidden = FALSE)
+#  scatter <- function() {
+#    req(input$scatter_curve)
+#
+#    labels <- if (mod_group() == "all_mod") NULL else
+#      setNames(paste(mod_data()[[mod_group()]], "  "), mod_data()[[mod_group()]])
+#
+#    guide <- if (mod_group() == "all_mod") FALSE else "legend"
+#    p <- ggplot(mod_data(), aes_string(x = "mean_age_months", y = es(),
+#                                       colour = mod_group())) +
+#      geom_jitter(aes(size = n, text = paste(short_cite, expt_num)), alpha = 0.5) +
+#      geom_hline(yintercept = 0, linetype = "dashed", color = "grey") +
+#      scale_colour_solarized(name = "", labels = labels, guide = guide) +
+#      scale_size_continuous(guide = FALSE) +
+#      xlab("\nMean Subject Age (Months)") +
+#      ylab("Effect Size\n")
+#
+#    #curve <- if (is.null(categorical_mods())) input$scatter_curve else "lm"
+#    if (input$scatter_curve == "lm") {
+#      p <- p + geom_smooth(aes_string(weight = sprintf("1 / %s", es_var())),
+#                      method = "lm", se = FALSE)
+#    } else if (input$scatter_curve == "loess") {
+#      p <- p + geom_smooth(aes_string(weight = sprintf("1 / %s", es_var())),
+#                      method = "loess", se = FALSE, span = 1)
+#    }
+#
+#   p <- ggplotly(p, tooltip = c("text"))
+#
+#   if (mod_group() != "all_mod") {
+#     p
+#   } else {
+#     p %>%
+#       layout(showlegend = FALSE)
+#   }
+#
+# }
+#
+# output$scatter <- renderPlotly(scatter())
+#
+# output$longitudinal <- reactive({
+#   req(input$dataset_name)
+#   filter(dataset_info, name == input$dataset_name)$longitudinal
+# })
+#
+# outputOptions(output, "longitudinal", suspendWhenHidden = FALSE)
 
   ########### VIOLIN PLOT ###########
 
